a426167979c8d131ad8cea686642753837614202
Ricardo Alberto <defcon1@Ricardos-MacBook-Pro.local>
2016-10-25 17:05:26 -0400

    AngularJS Experiment Hockey Stick Directory

---
+++ js/controllers.js
@@ -0,0 +1,29 @@
+var stickControllers = angular.module('stickControllers', ['ngAnimate']);
+
+
+stickControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
+    $http.get('js/data.json').success(function(data) {
+      $scope.sticks = data;
+      $scope.stickOrder = 'name';
+  });
+}]);
+
+stickControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+    $http.get('js/data.json').success(function(data) {
+      $scope.sticks = data;
+      $scope.whichItem = $routeParams.itemId;
+
+      if ($routeParams.itemId > 0) {
+        $scope.prevItem = Number($routeParams.itemId)-1;
+      } else {
+        $scope.prevItem = $scope.sticks.length-1;
+      }
+
+      if ($routeParams.itemId < $scope.sticks.length-1) {
+        $scope.nextItem = Number($routeParams.itemId)+1;
+      } else {
+        $scope.nextItem = 0;
+      }
+  
+  });
+}]);
